24f8287fd2f5a5b0f65256312e19ca8e15adda45
El asas Mohamed <assass.mohamed@gmail.com>
2017-12-20 02:06:12 +0000

    les méthodes signin et signup de le web api sont créés.

---
+++ LeKioskFullStack/Controllers/UtilisateursController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using LeKioskDAO;
+
+namespace LeKioskFullStack.Controllers
+{
+    public class UtilisateursController : ApiController
+    {
+        private KioskEntities db = new KioskEntities();
+
+
+        // GET: api/Utilisateurs
+        
+        public IHttpActionResult GetUtilisateurs()
+        {                      
+            return Ok(db.Utilisateurs.ToList());
+        }
+
+
+        //La méthode Signin qui prends deux paramètres (email et password) renvois un objet 'Utilisateur'
+        //si les information d'authentification sont correct ou un produit une erreur sinon.
+
+        // GET: api/Utilisateurs
+
+        [HttpGet]
+        [ResponseType(typeof(Utilisateur))]        
+        public IHttpActionResult Signin(string email, string password)
+        {
+            Utilisateur user = db.Utilisateurs.Where(u => u.email == email).Where(u => u.password == password).First();
+
+            if (user == null)
+            {                
+                return NotFound();
+            }
+
+            return Ok(user);
+
+        }
+
+
+
+        //La méthode Signup permet d'enregistrer un Utilisateur dans la base de données.
+        //l'objet utilisateur est recupéré depuis la requête.
+
+        // POST: api/Utilisateurs
+        [HttpPost]
+        [ResponseType(typeof(Utilisateur))]        
+        public HttpResponseMessage Signup(Utilisateur utilisateur)
+        {
+                       
+            try
+            {
+                db.Utilisateurs.Add(utilisateur);
+                db.SaveChanges();
+                var message = Request.CreateResponse(HttpStatusCode.Created, utilisateur);
+                
+                message.Headers.Location = new Uri(Request.RequestUri +"/"+ utilisateur.Id);
+                return message;
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest , ex);
+            }
+
+            
+        }
+
+       
+    }
+}
