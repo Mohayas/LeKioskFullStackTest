c1b3e053fd7a090a22d29fea2d0d58c77b1020bb
El asas Mohamed <assass.mohamed@gmail.com>
2017-12-21 12:49:16 +0000

    Add test units for Utilisateur DAL

---
+++ LeKiosk.FullStack.DAO.UnitTest/UtilisateurDALTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeKioskDAO;
+using System.Collections.Generic;
+
+namespace LeKiosk.FullStack.DAO.UnitTest
+{
+    [TestClass]
+    public class UtilisateurDALTest
+    {
+        UtilisateurDAL utilisateurDAL;
+
+        private void initialize()
+        {
+            utilisateurDAL = new UtilisateurDAL();
+        }
+
+        [TestMethod]
+        public void getListUtilisateursSuccess()
+        {
+            initialize();
+            List<Utilisateur> listUsers = utilisateurDAL.getListUtilisateurs();
+            Assert.AreNotEqual(listUsers.Count, 0);
+        }
+        /// <summary>
+        /// Test Methode getListUtilisateurs when Failed
+        /// </summary>
+        [TestMethod]
+        public void getListUtilisateursFailed()
+        {
+            initialize();
+            List<Utilisateur> listUsers = utilisateurDAL.getListUtilisateurs();
+            Assert.AreEqual(listUsers.Count, 0);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void getUtilisateurbyEmailSuccess()
+        {
+            initialize();
+            Utilisateur user = utilisateurDAL.getUtilisateurByEmail("asas.mohamed@gmail.com");
+            Assert.IsNotNull(user);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void getUtilisateurbyEmailFailed()
+        {
+            initialize();
+            Utilisateur user = utilisateurDAL.getUtilisateurByEmail("something@gmail.com");
+            Assert.IsNull(user);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void getUtilisateurbyEmailandPasswordSuccess()
+        {
+            initialize();
+            Utilisateur user = utilisateurDAL.getUtilisateurByEmailAndPasswword("asas.mohamed@gmail.com", "p@ssW0rd");
+            Assert.IsNotNull(user);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void getUtilisateurbyEmailandPasswordFailed()
+        {
+            initialize();
+            Utilisateur user = utilisateurDAL.getUtilisateurByEmailAndPasswword("something@gmail.com", "BadPassword");
+            Assert.IsNull(user);
+        }
+
+    }
+}
