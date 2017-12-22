d97757b6a1617493a74cdbc273f7576f4902b457
El asas Mohamed <assass.mohamed@gmail.com>
2017-12-22 05:51:40 +0000

    DB.sql , last commit

---
+++ DB.sql
@@ -0,0 +1,23 @@
+CREATE TABLE [dbo].[Utilisateur] (
+    [Id]         INT          IDENTITY (1, 1) NOT NULL,
+    [first_name] VARCHAR (50) NULL,
+    [last_name]  VARCHAR (50) NULL,
+    [email]      VARCHAR (50) NULL,
+    [password]   VARCHAR (50) NULL
+);
+
+
+
+INSERT INTO [dbo].[Utilisateur] ( [first_name], [last_name], [email], [password]) VALUES (N'moha', N'yas', N'mo@yas.com', N'aaa')
+INSERT INTO [dbo].[Utilisateur] ( [first_name], [last_name], [email], [password]) VALUES (N'me', N'me', N'me@me.com', N'aaa')
+INSERT INTO [dbo].[Utilisateur] ( [first_name], [last_name], [email], [password]) VALUES (N'ooo', N'ooo', N'ooo@oo.com', N'aaa')
+INSERT INTO [dbo].[Utilisateur] ( [first_name], [last_name], [email], [password]) VALUES (N'ooo', N'ooo', N'ooo@oo.com', N'aaa')
+INSERT INTO [dbo].[Utilisateur] ( [first_name], [last_name], [email], [password]) VALUES (N'uyu', N'yuy', N'az@yas.com', N'aaa')
+INSERT INTO [dbo].[Utilisateur] ( [first_name], [last_name], [email], [password]) VALUES (N'ttt', N'ttt', N'tt@tt.t', N'ttt')
+INSERT INTO [dbo].[Utilisateur] ( [first_name], [last_name], [email], [password]) VALUES (N'ana', N'ana', N'aa@aaa.coom', N'aaa')
+INSERT INTO [dbo].[Utilisateur] ( [first_name], [last_name], [email], [password]) VALUES (N'bbb', N'bbb', N'bb@bbb.bb', N'bbb')
+INSERT INTO [dbo].[Utilisateur] ( [first_name], [last_name], [email], [password]) VALUES (N'oo', N'oo', N'oo@kjk.oo', N'ooo')
+INSERT INTO [dbo].[Utilisateur] ( [first_name], [last_name], [email], [password]) VALUES (N'qq', N'qq', N'qq@qq.com', N'qqq')
+INSERT INTO [dbo].[Utilisateur] ( [first_name], [last_name], [email], [password]) VALUES (N'hghg', N'hghg', N'pp@pp.pp', N'ppp')
+INSERT INTO [dbo].[Utilisateur] ( [first_name], [last_name], [email], [password]) VALUES (N'ca', N'ca', N'cacayassen@gmail.com', N'ooo')
+
